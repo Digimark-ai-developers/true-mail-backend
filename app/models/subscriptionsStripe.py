e9e3a416375f2000ef8eab90d5c23bb7e2bf9e84
mhussaindigimark <m_husain@digimarkdevelopers.com>
2025-04-30 00:11:52 -0700

    modles updated

--- app/models/subscriptionsStripe.py
+++ app/models/subscriptionsStripe.py
@@ -8,9 +8,9 @@ class SubscriptionsStripe(Base):
     __tablename__ = 'Subscriptions Stripe'
     
     subscription_Id = Column(BigInteger, primary_key=True)
-    userId = Column(Integer, ForeignKey('User.user_Id'), nullable=False)
-    stripeSubscriptionId = Column(Integer, nullable=False)
-    stripeCustomerId = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('User.user_Id'), nullable=False)
+    stripe_subscriptionId = Column(Integer, nullable=False)
+    stripe_customerId = Column(Integer, nullable=False)
     status = Column(Boolean, nullable=False)
     current_period_start = Column(DateTime(timezone=False), nullable=False)
     current_period_end = Column(DateTime(timezone=False), nullable=False)
