9b849a4d8034b3a11e682bdb90db1f6018d6839b
Muhammad Umair <umair_yousaf@digimarkdevelopers.com>
2025-05-15 16:41:15 +0500

    added routers in main

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes import auth, subscription_stripe, user
+from app.routes import auth, subscription_stripe, user, email
 
 # from app.middlewares.auth_middleware import AuthMiddleware
 from app.database.db_config import create_database  # Import create_database function
@@ -36,6 +36,7 @@ app.add_middleware(
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(subscription_stripe.router)
+app.include_router(email.router)
 
 
 # Health Check Route
